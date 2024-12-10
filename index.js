8032df6795a6c2a6054f12c013dabb3fc8fbd7a0
Isaac <bennisaac24@gmail.com>
2024-12-10 13:34:53 +0300

    changed the classes to ids and added the selectors for the elements in the js

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// Select elements
+const dropdown = document.getElementById("dropdown");
+const itemName = document.getElementById("itemName");
+const itemQuantity = document.getElementById("itemQuantity");
+const addButton = document.getElementById("btn");
+const list = document.getElementById("list");
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("button");
+const clearItems = document.getElementById("clearBtn")
+
